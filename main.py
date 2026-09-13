02657e1ddc9ee08cc877070a7f4ba43ceddbff4c
macchupalabytutu-bit <macchupalabytutu@gmail.com>
2026-09-13 19:20:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def on_message(message):
         except Exception as e:
             print(f"Error en la API: {e}")
             try:
-                await message.reply("ERR_SYS_500... Mis circuitos fallaron... ¿Acaso la mención fue real?")
+                await message.reply("ERR_SYS_500... Mis circuitos fallaron... {e}")
             except Exception:
                 pass
 
